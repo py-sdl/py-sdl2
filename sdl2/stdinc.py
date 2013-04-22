b6d32ca828a8b2ebd5cf1fc6b6ae300db33b2579
Marcus von Appen <5445744+marcusva@users.noreply.github.com>
2013-04-22 09:15:57 +0200

    - fixed sdl2.pixels.SDL_Color type - fixed base type definitions in sdl2.stdinc

--- sdl2/stdinc.py
+++ sdl2/stdinc.py
@@ -1,7 +1,7 @@
 import sys
 from .dll import _bind
-from ctypes import c_int, c_byte, c_ubyte, c_ushort, c_short, c_uint, \
-    c_longlong, c_ulonglong, cdll, c_size_t, c_void_p, c_char_p
+from ctypes import c_int, c_int8, c_uint8, c_int16, c_uint16, c_int32, \
+    c_uint32, c_int64, c_uint64, cdll, c_size_t, c_void_p, c_char_p
 
 __all__ = ["SDL_FALSE", "SDL_TRUE", "SDL_bool", "Sint8", "Uint8", "Sint16",
            "Uint16", "Sint32", "Uint32", "Sint64", "Uint64", "SDL_malloc",
@@ -14,14 +14,14 @@ SDL_FALSE = 0
 SDL_TRUE = 1
 
 SDL_bool = c_int
-Sint8 = c_byte
-Uint8 = c_ubyte
-Sint16 = c_short
-Uint16 = c_ushort
-Sint32 = c_int
-Uint32 = c_uint
-Sint64 = c_longlong
-Uint64 = c_ulonglong
+Sint8 = c_int8
+Uint8 = c_uint8
+Sint16 = c_int16
+Uint16 = c_uint16
+Sint32 = c_int32
+Uint32 = c_uint32
+Sint64 = c_int64
+Uint64 = c_uint64
 
 _libc = None
 if sys.platform == "win32":
