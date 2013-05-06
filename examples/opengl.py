885813a3bd643292142c3edee731681995a4c4e7
Marcus von Appen <marcus@sysfault.org>
2013-05-06 08:28:35 +0200

    - fixed simple OpenGL example

--- examples/opengl.py
+++ examples/opengl.py
@@ -1,41 +1,61 @@
 """OpenGL rendering simulation"""
 import sys
-import random
+import ctypes
 
 try:
-    import mule.video as video
-    import mule.sdl.events as sdlevents
-    import mule.sdl.timer as sdltimer
-    import mule.sdl.render as sdlrender
-    import mule.sdl.video as sdlvideo
-    import mule.sdl.rect as sdlrect
+    from OpenGL import GL, GLU
+    from sdl2 import *
 except ImportError:
     import traceback
     traceback.print_exc()
     sys.exit(1)
 
-# Import the resources, so we have easy access to the example images.
-from sdl2.ext import Resources
-RESOURCES = Resources(__file__, "resources")
 
 def run():
-    # Initialize the video subsystem, create a window and make it visible.
-    video.init()
-    window = video.Window("Particles", size=(800, 600),
-                          flags=sdlvideo.SDL_WINDOW_OPENGL)
-    window.show()
+    if SDL_Init(SDL_INIT_VIDEO) != 0:
+        print SDL_GetError()
+        return -1
 
-    renderer = video.RenderContext(window)
+    window = SDL_CreateWindow(b"OpenGL demo", SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED, 800, 600,
+                              SDL_WINDOW_OPENGL)
+    if not window:
+        print(SDL_GetError())
+        return -1
 
-    # The almighty event loop. You already know several parts of it.
+    context = SDL_GL_CreateContext(window)
+
+    GL.glMatrixMode(GL.GL_PROJECTION | GL.GL_MODELVIEW)
+    GL.glLoadIdentity()
+    GL.glOrtho(-400, 400, 300, -300, 0, 1)
+
+    x = 0.0
+    y = 30.0
+
+    event = SDL_Event()
     running = True
     while running:
-        for event in video.get_events():
-            if event.type == sdlevents.SDL_QUIT:
+        while SDL_PollEvent(ctypes.byref(event)) != 0:
+            if event.type == SDL_QUIT:
                 running = False
-                break
 
-    video.quit()
+        GL.glClearColor(0, 0, 0, 1)
+        GL.glClear(GL.GL_COLOR_BUFFER_BIT)
+        GL.glRotatef(10.0, 0.0, 0.0, 1.0)
+        GL.glBegin(GL.GL_TRIANGLES)
+        GL.glColor3f(1.0, 0.0, 0.0)
+        GL.glVertex2f(x, y + 90.0)
+        GL.glColor3f(0.0, 1.0, 0.0)
+        GL.glVertex2f(x + 90.0, y - 90.0)
+        GL.glColor3f(0.0, 0.0, 1.0)
+        GL.glVertex2f(x - 90.0, y - 90.0)
+        GL.glEnd()
+
+        SDL_GL_SwapWindow(window)
+        SDL_Delay(10)
+    SDL_GL_DeleteContext(context)
+    SDL_DestroyWindow(window)
+    SDL_Quit()
     return 0
 
 if __name__ == "__main__":
