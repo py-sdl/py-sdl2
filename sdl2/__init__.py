c46d5c64c29d4ef66ee1d638a5ae9a7fa00cf83e
Marcus von Appen <marcus@sysfault.org>
2013-09-01 14:41:29 +0200

    - release 0.6.0

--- sdl2/__init__.py
+++ sdl2/__init__.py
@@ -61,5 +61,5 @@ SDL_QuitSubSystem = _bind("SDL_QuitSubSystem", [Uint32])
 SDL_WasInit = _bind("SDL_WasInit", [Uint32], Uint32)
 SDL_Quit = _bind("SDL_Quit")
 
-__version__ = "0.5.0"
-version_info = (0, 5, 0, "")
+__version__ = "0.6.0"
+version_info = (0, 6, 0, "")
