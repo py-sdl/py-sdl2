0d28a09f20137b4619415afaac15d35b5d0f7dd3
Kenneth Long <klong15@mail.greenriver.edu>
2015-05-06 19:51:37 -0700

    Fixes for #82 and #83 issues submitted to Marcus' py-sdl2 (byteify prevents zipfile bytesio usage and examples/gui.py edit event to text event)

--- sdl2/ext/image.py
+++ sdl2/ext/image.py
@@ -43,8 +43,10 @@ def load_image(fname, enforce=None):
     """
     if enforce is not None and enforce not in ("PIL", "SDL"):
         raise ValueError("enforce must be either 'PIL' or 'SDL', if set")
-
-    name = byteify(fname, "utf-8")
+    
+    if hasattr(fname, 'encode'):
+        name = byteify(fname, "utf-8")
+    
     if not _HASPIL and not _HASSDLIMAGE:
         imgsurface = surface.SDL_LoadBMP(name)
         if not imgsurface:
