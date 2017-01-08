20a17e89699bf713dfd8bc3d4b77db98ffd3e6ca
Marcus von Appen <marcus@sysfault.org>
2017-01-08 09:40:00 +0000

    - another year...

--- doc/conf.py
+++ doc/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'PySDL2'
-copyright = u'2013-2016, Marcus von Appen'
+copyright = u'2013-2017, Marcus von Appen'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
