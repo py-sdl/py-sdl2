ca71444a0a4c73fb218c49e064085071bd8966d4
Marcus von Appen <marcus@sysfault.org>
2017-07-21 07:20:33 +0100

    - Added Python 3.6 test support

--- make.bat
+++ make.bat
@@ -8,22 +8,16 @@
 @IF "%PYTHONPATH%" == "" SET PYTHONPATH=%CHDIR%
 @IF "%PYTHON27_X86%" == "" SET PYTHON27_X86=c:\Python27-x86\python.exe
 @IF "%PYTHON27_X64%" == "" SET PYTHON27_X64=c:\Python27-x64\python.exe
-@IF "%PYTHON32_X86%" == "" SET PYTHON32_X86=c:\Python32-x86\python.exe
-@IF "%PYTHON32_X64%" == "" SET PYTHON32_X64=c:\Python32-x64\python.exe
-@IF "%PYTHON33_X86%" == "" SET PYTHON33_X86=c:\Python33-x86\python.exe
-@IF "%PYTHON33_X64%" == "" SET PYTHON33_X64=c:\Python33-x64\python.exe
-@IF "%PYTHON34_X86%" == "" SET PYTHON34_X86=c:\Python34-x86\python.exe
-@IF "%PYTHON34_X64%" == "" SET PYTHON34_X64=c:\Python34-x64\python.exe
 @IF "%PYTHON35_X86%" == "" SET PYTHON35_X86=c:\Python35-x86\python.exe
 @IF "%PYTHON35_X64%" == "" SET PYTHON35_X64=c:\Python35-x64\python.exe
+@IF "%PYTHON36_X86%" == "" SET PYTHON36_X86=c:\Python36-x86\python.exe
+@IF "%PYTHON36_X64%" == "" SET PYTHON36_X64=c:\Python36-x64\python.exe
 @IF "%PYTHON%" == "" SET PYTHON=%PYTHON27_X64%
-@IF "%PYPY2%" == "" SET PYPY2=c:\pypy2-v5.4.1\pypy.exe
-@IF "%PYPY3%" == "" SET PYPY3=c:\pypy3-v2.4\pypy.exe
+@IF "%PYPY2%" == "" SET PYPY2=c:\pypy2-v5.7.1\pypy.exe
+@REM IF "%PYPY3%" == "" SET PYPY3=c:\pypy3-v2.4\pypy.exe
 
-@SET INTERP_X64=%PYTHON27_X64%;%PYTHON32_X64%;%PYTHON33_X64%;%PYTHON34_X64%;^
- %PYTHON35_X64%
-@SET INTERP_X86=%PYTHON27_X86%;%PYTHON32_X86%;%PYTHON33_X86%;%PYTHON34_X86%;^
- %PYTHON35_X86%;%PYPY2%;%PYPY3%
+@SET INTERP_X64=%PYTHON27_X64%;%PYTHON35_X64%;%PYTHON36_X64%
+@SET INTERP_X86=%PYTHON27_X86%;%PYTHON35_X86%;%PYTHON36_X86%;%PYPY2%
 @SET INTERPRETERS=%INTERP_X86%;%INTERP_X64%
 
 @IF "%~1" == "" GOTO :all
@@ -117,16 +111,12 @@
 @echo Deleting data...
 @RMDIR /S /Q C:\Python27-x86\Lib\site-packages\sdl2
 @RMDIR /S /Q C:\Python27-x64\Lib\site-packages\sdl2
-@RMDIR /S /Q C:\Python32-x86\Lib\site-packages\sdl2
-@RMDIR /S /Q C:\Python32-x64\Lib\site-packages\sdl2
-@RMDIR /S /Q C:\Python33-x86\Lib\site-packages\sdl2
-@RMDIR /S /Q C:\Python33-x64\Lib\site-packages\sdl2
-@RMDIR /S /Q C:\Python34-x86\Lib\site-packages\sdl2
-@RMDIR /S /Q C:\Python34-x64\Lib\site-packages\sdl2
 @RMDIR /S /Q C:\Python35-x86\Lib\site-packages\sdl2
 @RMDIR /S /Q C:\Python35-x64\Lib\site-packages\sdl2
-@RMDIR /S /Q C:\pypy2-v5.4.1\site-packages\sdl2
-@RMDIR /S /Q C:\pypy3-2.4\site-packages\sdl2
+@RMDIR /S /Q C:\Python36-x86\Lib\site-packages\sdl2
+@RMDIR /S /Q C:\Python36-x64\Lib\site-packages\sdl2
+@RMDIR /S /Q C:\pypy2-v5.7.1\site-packages\sdl2
+@REM RMDIR /S /Q C:\pypy3-2.4\site-packages\sdl2
 @echo done
 @GOTO :eof
 
