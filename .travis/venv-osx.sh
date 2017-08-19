8f9388164e4b0d6e7827302870f069b907775cee
Marcus von Appen <marcus@sysfault.org>
2017-08-19 10:52:26 +0100

    - fix pip command for python2 on OSX

--- .travis/venv-osx.sh
+++ .travis/venv-osx.sh
@@ -5,6 +5,6 @@ if [[ "$PYVER" == "3" ]]; then
     pip3 install virtualenv
     python3 -m virtualenv pysdl2-venv
 else
-    pip install virtualenv
+    pip2 install virtualenv
     python -m virtualenv pysdl2-venv
 fi
