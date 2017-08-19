19f37c1f81e3d2cc6c210c5138a5c7bbe6633528
Marcus von Appen <marcus@sysfault.org>
2017-08-19 10:11:02 +0100

    - naive attempt to uprade required packages on OSX

--- .travis/install-osx-deps.sh
+++ .travis/install-osx-deps.sh
@@ -11,18 +11,18 @@ fi
 
 brew install sdl2
 
-brew install jpeg
-brew install libpng
-brew install libtiff
-brew install webp
+brew install jpeg || brew upgrade jpeg
+brew install libpng || brew upgrade libpng
+brew install libtiff || brew upgrade libtiff
+brew install webp || brew upgrade webp
 brew install sdl2_image
 
-brew install freetype
+brew install freetype || brew upgrade freetype
 brew install sdl2_ttf
 
-brew install flac
-brew install libmikmod
-brew install libvorbis
+brew install flac || brew upgrade flac
+brew install libmikmod || brew upgrade libmikmod
+brew install libvorbis || brew upgrade libvorbis
 brew install sdl2_mixer
 
 brew install sdl2_gfx
