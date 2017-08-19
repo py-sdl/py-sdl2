30d49a511e1eb42cd5f241b8170cf5cfbf652cda
Marcus von Appen <marcus@sysfault.org>
2017-08-19 10:26:32 +0100

    - switch to the proper venv on OSX

---
+++ .travis/venv-osx.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+if [[ "$PYVER" == "3" ]]; then
+    pip3 install virtualenv
+    python3 -m virtualenv pysdl2-venv
+else
+    pip install virtualenv
+    python -m virtualenv pysdl2-venv
+fi
