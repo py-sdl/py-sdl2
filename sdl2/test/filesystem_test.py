1284fb8be991e68ae5b783b9e2d80721efab1e50
Austin Hurst <mynameisaustinhurst@gmail.com>
2020-01-14 22:44:59 -0500

    Fix GetPrefPath & GetBasePath return values

--- sdl2/test/filesystem_test.py
+++ sdl2/test/filesystem_test.py
@@ -11,16 +11,16 @@ class SDLFileSystemTest(unittest.TestCase):
     def test_SDL_GetBasePath(self):
         execprefix = sys.exec_prefix
         path = filesystem.SDL_GetBasePath()
-        ppath = cast(path, c_char_p).value
-        ppath = ppath.decode("utf-8")
+        path = path.decode("utf-8")
         if sys.version_info[0] < 3:
             execprefix = unicode(execprefix)
-        self.assertTrue(execprefix.lower() in ppath.lower())
-        SDL_free(path)
+        self.assertTrue(execprefix.lower() in path.lower())
 
-    @unittest.skip("not implemented")
     def test_SDL_GetPrefPath(self):
-        pass
+        path = filesystem.SDL_GetPrefPath(b"OrgName", b"AppName")
+        path = path.decode("utf-8")
+        self.assertTrue("OrgName" in path)
+        self.assertTrue("AppName" in path)
 
 
 if __name__ == '__main__':
