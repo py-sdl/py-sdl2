61661bd3ed177237000c21bf8d283f1536408784
Austin Hurst <mynameisaustinhurst@gmail.com>
2022-01-10 12:09:06 -0500

    Update examples to use new rendering API

--- examples/helloworld.py
+++ examples/helloworld.py
@@ -29,50 +29,43 @@ def run():
     # after creation. Thus we need to tell it to be shown now.
     window.show()
 
-    # Create a sprite factory that allows us to create visible 2D elements
-    # easily. Depending on what the user chosses, we either create a factory
-    # that supports hardware-accelerated sprites or software-based ones.
-    # The hardware-accelerated SpriteFactory requres a rendering context
-    # (or SDL_Renderer), which will create the underlying textures for us.
+    # Create a Renderer for the new window, which we can use to copy and
+    # draw things to the screen. Renderers can use hardware-accelerated
+    # backends (e.g. OpenGL, Direct3D) as well as software-accelerated ones,
+    # depending on the flags you create it with.
+    renderflags = sdl2.SDL_RENDERER_SOFTWARE
     if "-hardware" in sys.argv:
-        print("Using hardware acceleration")
-        renderer = sdl2.ext.Renderer(window)
-        factory = sdl2.ext.SpriteFactory(sdl2.ext.TEXTURE, renderer=renderer)
-    else:
-        print("Using software rendering")
-        factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
+        renderflags = (
+            sdl2.SDL_RENDERER_ACCELERATED | sdl2.SDL_RENDERER_PRESENTVSYNC
+        )
+    renderer = sdl2.ext.Renderer(window, flags=renderflags)
 
-    # Creates a simple rendering system for the Window. The
-    # SpriteRenderSystem can draw Sprite objects on the window.
-    spriterenderer = factory.create_sprite_render_system(window)
+    # Import an image file and convert it to a Texture. A Texture is an SDL
+    # surface that has been prepared for use with a given Renderer. 
+    tst_img = sdl2.ext.load_bmp(RESOURCES.get_path("hello.bmp"))
+    tx = sdl2.ext.Texture(renderer, tst_img)
 
-    # Creates a new 2D pixel-based surface to be displayed, processed or
-    # manipulated. We will use the one of the shipped example images
-    # from the resource package to display.
-    sprite = factory.from_image(RESOURCES.get_path("hello.bmp"))
+    # Display the image on the window. This code takes the texture we created
+    # earlier and copies it to the renderer (with the top-left corner of the
+    # texture placed at the coordinates (0, 0) on the window surface), then 
+    # takes the contents of the renderer surface and presents them on its
+    # associated window.
+    renderer.copy(tx, dstrect=(0, 0))
+    renderer.present()
 
-    # Display the surface on the window. This will copy the contents
-    # (pixels) of the surface to the window. The surface will be
-    # displayed at surface.position on the window. Play around with the
-    # surface.x and surface.y values or surface.position (which is just
-    # surface.x and surface.y grouped as tuple)!
-    spriterenderer.render(sprite)
+    # Create a simple event loop. This fetches the SDL2 event queue and checks
+    # for any quit events. Once a quit event is received, the loop will end
+    # and we'll send the signal to quit the program.
+    running = True
+    while running:
+        events = sdl2.ext.get_events()
+        for event in events:
+            if event.type == sdl2.SDL_QUIT:
+                running = False
+                break
 
-    # Set up an example event loop processing system. This is a necessity,
-    # so the application can exit correctly, mouse movements, etc. are
-    # recognised and so on. The TestEventProcessor class is just for
-    # testing purposes and does not do anything meaningful.  Take a look
-    # at its code to better understand how the event processing can be
-    # done and customized!
-    processor = sdl2.ext.TestEventProcessor()
-
-    # Start the event processing. This will run in an endless loop, so
-    # everything following after processor.run() will not be executed
-    # before some quitting event is raised.
-    processor.run(window)
-
-    # We called video.init(), so we have to call video.quit() as well to
-    # release the resources hold by the SDL DLL.
+    # Now that we're done with the SDL2 library, we shut it down nicely using
+    # the `sdl2.ext.quit` function.
     sdl2.ext.quit()
     return 0
 
