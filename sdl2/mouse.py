93332c90b08ab382592513b788c5646e61164b3b
Austin Hurst <mynameisaustinhurst@gmail.com>
2022-01-16 14:16:19 -0400

    Clean up mouse module

--- sdl2/mouse.py
+++ sdl2/mouse.py
@@ -42,7 +42,7 @@ __all__ = [
 class SDL_Cursor(c_void_p):
     pass
 
-
+SDL_SystemCursor = c_int
 SDL_SYSTEM_CURSOR_ARROW = 0
 SDL_SYSTEM_CURSOR_IBEAM = 1
 SDL_SYSTEM_CURSOR_WAIT = 2
@@ -56,7 +56,23 @@ SDL_SYSTEM_CURSOR_SIZEALL = 9
 SDL_SYSTEM_CURSOR_NO = 10
 SDL_SYSTEM_CURSOR_HAND = 11
 SDL_NUM_SYSTEM_CURSORS = 12
-SDL_SystemCursor = c_int
+
+SDL_MouseWheelDirection = c_int
+SDL_MOUSEWHEEL_NORMAL = 0
+SDL_MOUSEWHEEL_FLIPPED = 1
+
+SDL_BUTTON = lambda X: (1 << ((X) - 1))
+SDL_BUTTON_LEFT = 1
+SDL_BUTTON_MIDDLE = 2
+SDL_BUTTON_RIGHT = 3
+SDL_BUTTON_X1 = 4
+SDL_BUTTON_X2 = 5
+SDL_BUTTON_LMASK = SDL_BUTTON(SDL_BUTTON_LEFT)
+SDL_BUTTON_MMASK = SDL_BUTTON(SDL_BUTTON_MIDDLE)
+SDL_BUTTON_RMASK = SDL_BUTTON(SDL_BUTTON_RIGHT)
+SDL_BUTTON_X1MASK = SDL_BUTTON(SDL_BUTTON_X1)
+SDL_BUTTON_X2MASK = SDL_BUTTON(SDL_BUTTON_X2)
+
 SDL_GetMouseFocus = _bind("SDL_GetMouseFocus", None, POINTER(SDL_Window))
 SDL_GetMouseState = _bind("SDL_GetMouseState", [POINTER(c_int), POINTER(c_int)], Uint32)
 SDL_GetRelativeMouseState = _bind("SDL_GetRelativeMouseState", [POINTER(c_int), POINTER(c_int)], Uint32)
@@ -74,17 +90,3 @@ SDL_ShowCursor = _bind("SDL_ShowCursor", [c_int], c_int)
 SDL_WarpMouseGlobal = _bind("SDL_WarpMouseGlobal", [c_int, c_int], c_int, added='2.0.4')
 SDL_CaptureMouse = _bind("SDL_CaptureMouse", [SDL_bool], c_int, added='2.0.4')
 SDL_GetGlobalMouseState = _bind("SDL_GetGlobalMouseState", [POINTER(c_int), POINTER(c_int)], Uint32, added='2.0.4')
-SDL_BUTTON = lambda X: (1 << ((X) - 1))
-SDL_BUTTON_LEFT = 1
-SDL_BUTTON_MIDDLE = 2
-SDL_BUTTON_RIGHT = 3
-SDL_BUTTON_X1 = 4
-SDL_BUTTON_X2 = 5
-SDL_BUTTON_LMASK = SDL_BUTTON(SDL_BUTTON_LEFT)
-SDL_BUTTON_MMASK = SDL_BUTTON(SDL_BUTTON_MIDDLE)
-SDL_BUTTON_RMASK = SDL_BUTTON(SDL_BUTTON_RIGHT)
-SDL_BUTTON_X1MASK = SDL_BUTTON(SDL_BUTTON_X1)
-SDL_BUTTON_X2MASK = SDL_BUTTON(SDL_BUTTON_X2)
-SDL_MOUSEWHEEL_NORMAL = 0
-SDL_MOUSEWHEEL_FLIPPED = 1
-SDL_MouseWheelDirection = c_int
