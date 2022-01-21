fbc987654b50a24d8b33bd5a42c5fa4150599ae2
Austin Hurst <mynameisaustinhurst@gmail.com>
2022-01-20 20:37:37 -0400

    Add basic conftest module

---
+++ sdl2/test/conftest.py
@@ -0,0 +1,13 @@
+# pytest configuration file
+import pytest
+import sdl2
+
+
+@pytest.fixture(scope="module")
+def with_sdl():
+    sdl2.SDL_ClearError()
+    ret = sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO | sdl2.SDL_INIT_TIMER)
+    assert sdl2.SDL_GetError() == b""
+    assert ret == 0
+    yield
+    SDL_Quit()
