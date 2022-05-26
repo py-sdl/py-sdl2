d4234bb6e55052cd9998067fa56aab1d0c48b2b2
Simon McVittie <smcv@collabora.com>
2022-05-26 23:46:37 +0100

    power_test: Relax assertions about known information (#231)
    If a Linux laptop has been configured to stop charging the battery at
    a value below 100% (to increase battery longevity by preventing
    over-charging), and is charged to the configured maximum, SDL reports
    that as SDL_POWERSTATE_UNKNOWN but still reports a percentage.
    
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- sdl2/test/power_test.py
+++ sdl2/test/power_test.py
@@ -11,11 +11,17 @@ def test_SDL_GetPowerInfo():
         sdl2.SDL_POWERSTATE_CHARGING,
         sdl2.SDL_POWERSTATE_CHARGED
     ]
+    no_battery = [
+        sdl2.SDL_POWERSTATE_NO_BATTERY
+    ]
     remaining, pct = c_int(), c_int()
     state = sdl2.SDL_GetPowerInfo(byref(remaining), byref(pct))
     if state in has_battery:
         assert pct.value <= 100
         assert pct.value > 0
-    else:
+    elif state in no_battery:
         assert remaining.value == -1
         assert pct.value == -1
+    # else no assertion about remaining and pct:
+    # we can have state SDL_POWERSTATE_UNKNOWN and still know a percentage
+    # if a battery is present but is not being charged
